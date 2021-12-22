69c9698c83a47d70f8eea7a96007e4be44ab1454
avan-pra <avan-pra@users.noreply.github.com>
2021-12-23 00:45:12 +0100

    part 2

--- 2021/day10/main.py
+++ 2021/day10/main.py
@@ -1,7 +1,3 @@
-fd = open('./data', 'r')
-
-points = 0
-
 symbols = {
     ']': '[',
     ')': '(',
@@ -10,10 +6,10 @@ symbols = {
 }
 
 value = {
-    ']': 57,
-    ')': 3,
-    '}': 1197,
-    '>': 25137
+    '[': 2,
+    '(': 1,
+    '{': 3,
+    '<': 4
 }
 
 def get_validity(inp: list):
@@ -24,13 +20,36 @@ def get_validity(inp: list):
             mylist.append(c)
         else:
             if mylist.pop() != symbols[c]:
-                return value[c]
-
+                return 1
     return 0
 
+def autocomplete(inp: list):
+    mylist = []
+    score = 0
+
+    for c in inp:
+        if c == '[' or c == '(' or c == '{' or c == '<':
+            mylist.append(c)
+        else:
+            if mylist.pop() != symbols[c]:
+                print('BIG ERROR')
+
+    while len(mylist) != 0:
+        score = (score * 5) + value[mylist.pop()]
+    
+    return score
+
+fd = open('./data', 'r')
+
+points = []
 
 for line in fd.readlines():
     inp = [c for c in line.strip('\n')]
-    points += get_validity(inp)
+    if (get_validity(inp) == 0):
+        points.append(autocomplete(inp))
 
-print(points)
+import math
+
+points.sort()
+# print(points)
+print(points[math.floor(len(points) / 2)])
